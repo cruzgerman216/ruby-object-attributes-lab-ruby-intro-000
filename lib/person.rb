1fe9639e6379b0f4fccfd0bbd35dc5c31fe918ed
German Cruz <cruzgerman216@yahoo.com>
2020-02-23 00:02:17 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+  def initialize(name, job)
+    @name = name
+    @job = job
+  end
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+  def job
+    @job
+  end
+
+  def breed=(job)
+    @breed = job
+  end
+end
